2a7240cc5fffe8ae9d30b0c6689e758a26eeda3f
hsnAlwayel <hsn.alwayel@gmail.com>
2025-04-06 12:11:47 +0300

    All pets rendered from cat component using ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { pets } from '../data/pets';
-
+import { CatComponent } from './cat/cat.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CatComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'TASK-NG-Passing-Data';
